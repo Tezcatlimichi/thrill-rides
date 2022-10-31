13ae7c26ac9fe491802146ab081aaade41266545
Brian Lara <Briandlara1@gmail.com>
2022-10-31 17:15:26 -0400

    created a ride page

--- client/src/pages/Ride.js
+++ client/src/pages/Ride.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
 const Ride = () => {
-  return <div>Ride</div>
+  return (
+    <div>
+      <section className="featured-container">
+        <h3 id="featured-title">Title</h3>
+        <img
+          src="https://via.placeholder.com/1000x600/09f/fff"
+          alt="ride name"
+          className="featured-image"
+        />
+      </section>
+    </div>
+  )
 }
 
 export default Ride
