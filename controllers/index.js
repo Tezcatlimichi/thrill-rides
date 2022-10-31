0e99d24bb1c57996837704752189aba67123231c
Sarah Duperoux <Duperouxsarah@gmail.com>
2022-10-31 16:34:25 -0400

    added all of ride controllers and added cors and morgan to server.js file

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const Ride = require('../models/Ride')
+const Ticket = require('../models/Ticket')
 
 /// cruds go here
+//create
 const createRide = async (req, res) => {
   try {
     const ride = await new Ride(req.body)
@@ -12,7 +14,31 @@ const createRide = async (req, res) => {
     return res.status(500).json({ error: error.message })
   }
 }
+//get or read
+const getAllRides = async (req, res) => {
+  try {
+    const rides = await Ride.find()
+    return res.status(200).json({ rides })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+// delete
+const deleteRide = async (req, res) => {
+  try {
+    const { id } = req.params
+    const deleted = await Ride.findByIdAndDelete(id)
+    if (deleted) {
+      return res.status(200).send('Ride deleted')
+    }
+    throw new Error('Ride not found')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
 
 module.exports = {
-  createRide
+  createRide,
+  getAllRides,
+  deleteRide
 }
