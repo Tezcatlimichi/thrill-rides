2d8b3c4218b95da455e6c957bfc7770574a2a4d2
Sarah Duperoux <Duperouxsarah@gmail.com>
2022-10-31 16:02:27 -0400

    added first crud method

---
+++ controllers/index.js
@@ -0,0 +1,18 @@
+const Ride = require('../models/Ride')
+
+/// cruds go here
+const createRide = async (req, res) => {
+  try {
+    const ride = await new Ride(req.body)
+    await ride.save()
+    return res.status(201).json({
+      ride
+    })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+module.exports = {
+  createRide
+}
