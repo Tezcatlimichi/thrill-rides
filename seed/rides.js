c0a27cefc27e96419558353242855e9c543e71e0
Kathya Castaneda <kathyakathya.kc@gmail.com>
2022-10-31 17:44:25 -0600

    prepare for pull

--- seed/rides.js
+++ seed/rides.js
@@ -1,5 +1,5 @@
 const db = require('../db')
-const Ride = require('../models/Ride')
+const { Ride } = require('../models/Ride')
 
 // Connect to the database
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
