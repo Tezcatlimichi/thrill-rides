363f17fbe17ac40171401c4428b17980e763037a
Brian Lara <Briandlara1@gmail.com>
2022-11-01 16:01:35 -0400

    added more css moved the get tickets button location

--- client/src/components/Ride.jsx
+++ client/src/components/Ride.jsx
@@ -1,29 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Ride = (props) => {
   console.log(props)
-<<<<<<< HEAD
-  return (
-    <div>
-      <h1>{props.ride.name}</h1>
-      <h4>{props.ride.description}</h4>
-      {props.ride.images.map((image) => (
-        <img src={image} width="1000px" height="600px" />
-      ))}
-    </div>
-  )
-=======
+
   return (<div>
     <div id='ride-header'>
-      <h1 id='ride-name' className='font-1' >{props.ride.name}</h1>
       {props.ride.images.map((image)=>(
         <img src={image} alt={props.ride.name} width="1000px" height="600px" id='ride-image'/>
       ))} 
+      <div id='ride-text-container' > 
+      <h1 id='ride-name' className='font-1' >{props.ride.name}</h1>
+      <Link to={'/tickets'}>
+        <button type="button" id="get-ticket-button">
+          Get Tickets
+        </button>
+      </Link>
+      </div>
+
     </div>
 
       <h4 id='ride-description'>{props.ride.description}</h4>
   </div>)
->>>>>>> 292b9c563bbb1389bdcb4e705bf93ee28c7eb2b9
 }
 
 export default Ride
