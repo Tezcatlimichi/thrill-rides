7830662b1e1ec8cef7c80ef80050a6c98f47c292
Brian Lara <Briandlara1@gmail.com>
2022-11-02 00:17:06 -0400

    added heavy css on all pages

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,83 +1,10 @@
 import React from 'react'
 
-const Banner = ({image1}) => {
-
-    // let bannerPosition = 1;
-    // let bannerTimer = 6000;
-
-    // window.onload = function() {
-    //     bannerCarousel();
-    // }
-
-    
-    
-    
-    
-    // const bannerCarousel = () =>{
-    //     if(bannerPosition === 1){
-    //         document.getElementById('img-banner-2').style.opacity = '0';
-            
-    //         setTimeout(function () {
-    //             document.getElementById(`img-banner-1`).style.right = "0px";
-    //             document.getElementById(`img-banner-1`).style.zIndex = "100";
-    //             document.getElementById(`img-banner-2`).style.right = "-2000px";
-    //             document.getElementById(`img-banner-2`).style.zIndex = "1000";
-    //             document.getElementById(`img-banner-3`).style.right = "2000px";
-    //             document.getElementById(`img-banner-3`).style.zIndex = "10";
-    //         }, 500);
-            
-    //         setTimeout(function() {
-    //             document.getElementById('img-banner-2').style.opacity = '1';
-    //         }, 1000)
-    //         bannerPosition = 2;
-    //     }
-    //     else if(bannerPosition === 2){
-    //         document.getElementById('img-banner-3').style.opacity = '0';
-            
-    //         setTimeout(function () {
-    //             document.getElementById(`img-banner-2`).style.right = "0px";
-    //             document.getElementById(`img-banner-2`).style.zIndex = "100";
-    //             document.getElementById(`img-banner-3`).style.right = "-2000px";
-    //             document.getElementById(`img-banner-3`).style.zIndex = "1000";
-    //             document.getElementById(`img-banner-1`).style.right = "2000px";
-    //             document.getElementById(`img-banner-1`).style.zIndex = "10";
-    //         }, 500);
-            
-    //         setTimeout(function() {
-    //             document.getElementById('img-banner-3').style.opacity = '1';
-    //         }, 1000)
-    //         bannerPosition = 3;
-    //     }
-    //     else if(bannerPosition === 3){
-    //         document.getElementById('img-banner-1').style.opacity = '0';
-            
-    //         setTimeout(function () {
-    //             document.getElementById(`img-banner-3`).style.right = "0px";
-    //             document.getElementById(`img-banner-3`).style.zIndex = "100";
-    //             document.getElementById(`img-banner-1`).style.right = "-2000px";
-    //             document.getElementById(`img-banner-1`).style.zIndex = "1000";
-    //             document.getElementById(`img-banner-2`).style.right = "2000px";
-    //             document.getElementById(`img-banner-2`).style.zIndex = "10";
-    //         }, 500);
-            
-    //         setTimeout(function() {
-    //             document.getElementById('img-banner-1').style.opacity = '1';
-    //         }, 1000)
-    //         bannerPosition = 1;
-    //     }
-    // }
-    
-    // const startCarouselLoop = setInterval(function(){
-    //     bannerCarousel();
-    // }, bannerTimer)
-
-
-
+const Banner = ({image1, name, key}) => {
 
   return(
-    <div id="banner-container">
-        <img src={image1} alt="Skull Island: Reign of Kong" className='img-banner' id='img-banner-3'/>
-        
+    <div className='banner-ride inline-snap' key={key}>
+      <img src={image1} alt={name} id='img-banner' width="1550px" height="1000px"/>
     </div>
   )
 }
